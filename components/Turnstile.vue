26e6bedf18bfcc48bd249743ea187239dbab6808
Ganesh Annan <gannan@digitalbazaar.com>
2023-10-19 20:52:05 +0200

    Add initial implementation.

--- components/Turnstile.vue
+++ components/Turnstile.vue
@@ -1,8 +1,12 @@
 <template>
   <vue-turnstile
     v-model="token"
-    theme="light"
-    appearance="interaction-only"
+    :reset-interval="resetInterval"
+    :size="size"
+    :theme="theme"
+    :action="action"
+    :appearance="appearance"
+    :render-on-mount="renderOnMount"
     :site-key="siteKey" />
 </template>
 <script>
@@ -10,13 +14,55 @@
   * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
   */
 import {config} from '@bedrock/web';
+import {defineEmits, ref, watch} from 'vue';
 
 export default {
   name: 'Turnstile',
-  data() {
+
+  props: {
+    modelValue: {
+      type: String,
+      required: true,
+    },
+    resetInterval: {
+      type: Number,
+      required: false,
+    },
+    size: {
+      type: String,
+      required: false,
+    },
+    theme: {
+      type: String,
+      required: false,
+    },
+    action: {
+      type: String,
+      required: false,
+    },
+    appearance: {
+      type: String,
+      required: false,
+    },
+    renderOnMount: {
+      type: Boolean,
+      required: false,
+    },
+  },
+
+  setup() {
+    const token = ref('');
+    // siteKey is not a secret
+    const siteKey = ref(config.turnstile.siteKey);
+
+    const emit = defineEmits(['update:modelValue'])
+    watch(
+      () => token.value,
+      tokenValue => emit('update:modelValue', tokenValue));
+
     return {
-      // siteKey is not a secret
-      siteKey: config.turnstile.siteKey
+      token,
+      siteKey,
     }
   }
 };
