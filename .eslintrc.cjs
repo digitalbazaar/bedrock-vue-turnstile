f0ed89a768ac7135c5624fd0e070c19e165f8364
Matthew Collier <mcollier@digitalbazaar.com>
2023-10-19 13:49:15 -0400

    Add core files.

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    'digitalbazaar',
+    'digitalbazaar/jsdoc',
+    'digitalbazaar/vue'
+  ]
+};
---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# bedrock-vue-turnstile ChangeLog
+
+## 1.0.0 - TBD
+
+- See git history for changes.
