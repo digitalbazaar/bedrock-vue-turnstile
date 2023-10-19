2b38641c521b5e5124271ea3870afa958537d067
Matthew Collier <mcollier@digitalbazaar.com>
2023-10-19 13:49:15 -0400

    Add core files.

---
+++ components/Turnstile.vue
@@ -0,0 +1,24 @@
+<template>
+  <vue-turnstile
+    v-model="token"
+    theme="light"
+    appearance="interaction-only"
+    :site-key="siteKey" />
+</template>
+<script>
+/*!
+  * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
+  */
+import {config} from '@bedrock/web';
+
+export default {
+  name: 'Turnstile',
+  data() {
+    return {
+      // siteKey is not a secret
+      siteKey: config.turnstile.siteKey
+    }
+  }
+};
+
+</script>
