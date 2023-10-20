43c00787c8c908091d5d993a092c0208c48f018e
Mandy Venables <mandylvenables@gmail.com>
2023-10-20 11:01:39 -0400

    Add defaults.

--- components/Turnstile.vue
+++ components/Turnstile.vue
@@ -1,65 +1,65 @@
 <template>
   <VueTurnstile
     v-model="token"
+    :reset-interval="resetInterval"
+    :size="size"
+    :theme="theme"
+    :action="action"
+    :appearance="appearance"
+    :render-on-mount="renderOnMount"
     :site-key="siteKey" />
 </template>
-<script>
+
+<script setup>
 /*!
   * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
   */
 import {config} from '@bedrock/web';
-import {defineEmits, ref, watch} from 'vue';
+import {defineEmits, defineProps, ref, watch} from 'vue';
 import VueTurnstile from 'vue-turnstile';
 
-export default {
-  name: 'Turnstile',
-  components: {VueTurnstile},
-  props: {
-    modelValue: {
-      type: String,
-      required: true,
-    },
-    resetInterval: {
-      type: Number,
-      required: false,
-    },
-    size: {
-      type: String,
-      required: false,
-    },
-    theme: {
-      type: String,
-      required: false,
-    },
-    action: {
-      type: String,
-      required: false,
-    },
-    appearance: {
-      type: String,
-      required: false,
-    },
-    renderOnMount: {
-      type: Boolean,
-      required: false,
-    },
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: true,
   },
-
-  setup() {
-    const token = ref('');
-    // siteKey is not a secret
-    const siteKey = ref(config.turnstile.siteKey);
-
-    const emit = defineEmits(['update:modelValue'])
-    watch(
-      () => token.value,
-      tokenValue => emit('update:modelValue', tokenValue));
-
-    return {
-      token,
-      siteKey,
-    }
+  resetInterval: {
+    default: 295000,
+    type: Number,
+    required: false,
+  },
+  size: {
+    default: 'normal',
+    type: String,
+    required: false,
+  },
+  theme: {
+    default: 'auto',
+    type: String,
+    required: false,
+  },
+  action: {
+    default: '',
+    type: String,
+    required: false,
+  },
+  appearance: {
+    default: 'always',
+    type: String,
+    required: false,
+  },
+  renderOnMount: {
+    default: true,
+    type: Boolean,
+    required: false,
   }
-};
+});
+
+const token = ref('');
+// siteKey is not a secret
+const siteKey = ref(config.turnstile.siteKey);
+
+const emit = defineEmits(['update:modelValue'])
+watch(token, tokenValue => emit('update:modelValue', tokenValue));
 
 </script>
