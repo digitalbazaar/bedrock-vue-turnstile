b8bc30bb29d08205ed558bca957cab95c9796d40
Mandy Venables <mandylvenables@gmail.com>
2023-10-23 15:02:10 -0400

    Rename to `TurnstileForm`.

--- components/TurnstileForm.vue
+++ components/TurnstileForm.vue
@@ -14,11 +14,11 @@
 /*!
   * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
   */
-import {config} from '@bedrock/web';
 import {defineEmits, defineProps, ref, watch} from 'vue';
+import {config} from '@bedrock/web';
 import VueTurnstile from 'vue-turnstile';
 
-const props = defineProps({
+defineProps({
   modelValue: {
     type: String,
     required: true,
@@ -59,7 +59,7 @@ const token = ref('');
 // siteKey is not a secret
 const siteKey = ref(config.turnstile.siteKey);
 
-const emit = defineEmits(['update:modelValue'])
+const emit = defineEmits(['update:modelValue']);
 watch(token, tokenValue => emit('update:modelValue', tokenValue));
 
 </script>
